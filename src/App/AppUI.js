26e8791e6ea8c121d6aca01327406cd7d2757882
danyr <phoenixdhr@gmail.com>
2022-11-29 18:56:03 -0500

    se creo una nueva carptea con un archivo donde se uso CreateContext, ademas se separo el Hook creado llamado useItem, el archivo TodoContext/index.js es el que provee toda las variables ya sea de estado o variables simples

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,56 +1,50 @@
-import React from 'react'
+import React from "react";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import TodoList from "../TodoList";
 import CreateTodoButton from "../CreateTodoButton";
 import TodoItems from "../TodoItems";
+import { TodoContext } from "../TodoContext";
 
-
-function AppUI(
-    {
-    loading,
-    errores,
-    nTodos, 
-    nCompleted,
-    serchvalue, 
-    setSerchvalue,
-    todoSearchx, 
-    fonComplete,
-    fonDelete,}
-
-) {
-
-
-
+function AppUI() {
   return (
     <>
-      <TodoCounter 
-          nTodos={nTodos} 
-          nCompleted={nCompleted} />
+      <TodoCounter />
 
-      <TodoSearch 
-          serchvalue={serchvalue} 
-          setSerchvalue={setSerchvalue} />
-        
-      {loading&&<h2>cargando...</h2>}
-      {errores&&<h2>Hubo un error🙁</h2>}
-      {(!loading&&todoSearchx)&&<h2>Crea tu primer ToDo 😉</h2>}
+      <TodoSearch />
 
-      <TodoList>
-        {todoSearchx.map((todo) => (
-          <TodoItems
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            fonComplete={() => fonComplete(todo.text)}
-            fonDelete={() => fonDelete(todo)}
-          />
-        ))}
-      </TodoList>
+      <TodoContext.Consumer>
+        {({
+          loading,
+          errores,
+          todoSearchx,
+          completed,
+          fonComplete,
+          fonDelete,
+        }) => {
+          return (
+            <TodoList>
+              {loading && <h2>cargando...</h2>}
+              {errores && <h2>Hubo un error🙁</h2>}
+              {!loading && todoSearchx && <h2>Crea tu primer ToDo 😉</h2>}
+
+              {todoSearchx.map((todo) => (
+                <TodoItems
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}
+                  fonComplete={() => fonComplete(todo.text)}
+                  fonDelete={() => fonDelete(todo)}
+                />
+              ))}
+            </TodoList>
+          );
+        }}
+      </TodoContext.Consumer>
 
       <CreateTodoButton />
     </>
-  )
+  );
 }
 
-export {AppUI}
+export { AppUI };
--- src/App/index.js
+++ src/App/index.js
@@ -1,46 +1,7 @@
-import React, { useEffect } from "react";
+
 import "./App.css";
 import { AppUI } from "./AppUI";
-
-function useItem(itemName_V1, initial_Items) {
-  const [items, setItems] = React.useState(initial_Items);
-  const [loading, setloading] = React.useState(true);
-  const [errores, seterrores] = React.useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      try {
-        if (JSON.parse(localStorage.getItem(itemName_V1) == null)) {
-          initial_Items = initial_Items;
-        } else {
-          initial_Items = JSON.parse(localStorage.getItem(itemName_V1));
-        }
-        setloading(false);
-        setItems(initial_Items)
-
-      } catch (error) {
-        seterrores(error);
-      }
-    }, 1000);
-  }, []);
-
-  const setStorage = (params) => {
-    
-    try {
-      setItems(params);
-      localStorage.setItem(itemName_V1, JSON.stringify(params));
-      initial_Items = JSON.parse(localStorage.getItem(itemName_V1));
-    } catch (error) {
-      seterrores(error)
-    }
-    
-
-
-
-  };
-
-  return { items, setStorage, loading, errores };
-}
+import { TodoProvider } from "../TodoContext";
 
 const tododefault = [
   { text: "desayunar", completed: true },
@@ -52,61 +13,12 @@ const tododefault = [
 ];
 
 function App() {
-  const {
-    items: todos,
-    setStorage: setTodo,
-    loading,
-    errores,
-  } = useItem("todoinicial", []);
-
-  const [serchvalue, setSerchvalue] = React.useState("");
-
-  const nCompleted = todos.filter((todos) => todos.completed).length;
-  const nTodos = todos.length;
-
-  let todoSearchx = [];
-
-  if (serchvalue.length < 1) {
-    todoSearchx = todos;
-  } else {
-    todoSearchx = todos.filter((elementTodo) => {
-      const minSearchValue = serchvalue.toLowerCase();
-      const minelementTodo = elementTodo.text.toLowerCase();
-      return minelementTodo.includes(minSearchValue);
-    });
-  }
-
-  const fonComplete = (text) => {
-    const indiceElmentoTodo = todos.findIndex(
-      (elemnto) => elemnto.text === text
-    );
-    const nuevoTodos = [...todos];
-    nuevoTodos[indiceElmentoTodo].completed = true;
-    setTodo(nuevoTodos);
-  };
-
-  const fonDelete = (todo) => {
-    const indiceElmentoTodo = todos.findIndex(
-      (elemnto) => elemnto.text === todo.text
-    );
-    const nuevoTodos = [...todos];
-    nuevoTodos.splice(indiceElmentoTodo, 1);
-    setTodo(nuevoTodos);
-  };
-
   return (
-    <AppUI
-      loading={loading}
-      errores={errores}
-      nTodos={nTodos}
-      nCompleted={nCompleted}
-      serchvalue={serchvalue}
-      setSerchvalue={setSerchvalue}
-      todoSearchx={todoSearchx}
-      fonComplete={fonComplete}
-      fonDelete={fonDelete}
-    ></AppUI>
-  );
+  <TodoProvider>
+    <AppUI/>
+  </TodoProvider>
+  )
+  
 }
 
 export default App;
